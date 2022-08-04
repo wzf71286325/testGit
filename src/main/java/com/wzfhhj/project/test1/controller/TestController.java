d305d51dcaa0aa156a3f381442451294089f469c
Administrator <wzf13636376719@163.com>
2022-08-04 15:06:11 +0800

    test2

--- src/main/java/com/wzfhhj/project/test1/controller/TestController.java
+++ src/main/java/com/wzfhhj/project/test1/controller/TestController.java
@@ -4,22 +4,40 @@ package com.wzfhhj.project.test1.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class TestController {
 
 
+    private void testFun()
+    {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++)
+        {
+            list.add(i);
+        }
+    }
 
     @GetMapping("test/one")
     public String testOne()
     {
-        Collection a = new HashSet();
-
-        int[] b = new int[10];
-        b[10] = 10;
-
         return "ok";
     }
+
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    private void testMethod()
+    {
+        System.out.println("1111111111111111");
+    }
+
+
+
 }
