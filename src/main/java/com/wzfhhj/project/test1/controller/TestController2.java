2a7c9e8a1b1727e73d4984e2f6d5e09cbef92454
Administrator <wzf13636376719@163.com>
2022-08-04 15:11:22 +0800

    test-develop

---
+++ src/main/java/com/wzfhhj/project/test1/controller/TestController2.java
@@ -0,0 +1,43 @@
+package com.wzfhhj.project.test1.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class TestController2 {
+
+
+    private void testFun()
+    {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++)
+        {
+            list.add(i);
+        }
+    }
+
+    @GetMapping("test/one")
+    public String testOne()
+    {
+        return "ok";
+    }
+
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    private void testMethod()
+    {
+        System.out.println("1111111111111111");
+    }
+
+
+
+}
